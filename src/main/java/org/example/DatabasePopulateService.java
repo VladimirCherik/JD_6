184c31498a76d1444fe0d29c856c45e59015d621
VladimirCherik <vladimir_cherik@i.ua>
2023-12-22 19:58:08 +0200

    HW_6

---
+++ src/main/java/org/example/DatabasePopulateService.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabasePopulateService {
+    public static void main(String[] args){
+
+        SqlFileReader reader = new SqlFileReader();
+
+        try (Statement stmt = Database.getInstance().getConnection().createStatement()) {
+            stmt.execute(reader.sqlQueryFromFileReader("populate_db.sql"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
